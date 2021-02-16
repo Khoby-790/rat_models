cbf6acd53b62d47ef84bd928edcad33fa1aa3887
Khoby-790 <ebaidoo79@gmail.com>
2021-02-16 16:12:24 +0000

    --- Auto-Git Commit ---

--- scripts/lte_model.py
+++ scripts/lte_model.py
@@ -20,8 +20,6 @@ data["status"] = np.where(data["Radio_CQI_Distribution"] / 10000 > 30, 0, np.whe
     data["Radio_CQI_Distribution"] / 10000 > 15, 1, 2
 ))
 
-four_g_data["Signal_strength"] = [abs(Decimal(number))
-                                  for number in four_g_data["Signal_strength"]]
 
 four_g_data["status"] = np.where(four_g_data["Signal_strength"] > 30, 0, abs(
     four_g_data["Signal_strength"] > 15, 1, 2
