fc241d9bcbdbb7f49e24d94d5b697b84f8e1713c
Khoby-790 <ebaidoo79@gmail.com>
2021-02-16 18:16:43 +0000

    --- Auto-Git Commit ---

--- scripts/3g_model.py
+++ scripts/3g_model.py
@@ -38,7 +38,7 @@ train_data_4g, test_data_4g = xlib.split_by_fractions(four_g_data, [0.8, 0.2])
 train_labels_4g = train_data_4g["status"]
 test_labels_4g = test_data_4g["status"]
 
-X = np.array(train_data_4g)
+X = np.array(train_data_4g.values)
 
 normalize_4g = preprocessing.Normalization()
 normalize_4g.adapt(X)
@@ -46,8 +46,8 @@ normalize_4g.adapt(X)
 
 print("Creating new 4G model")
 model_4g = keras.Sequential([
-    # normalize_4g,
-    layers.Flatten(5),
+    normalize_4g,
+    # layers.Flatten(5),
     layers.Dense(16, activation="relu"),
     layers.Dense(18, activation="softmax"),
     layers.Dense(3, activation="softmax"),
@@ -55,4 +55,4 @@ model_4g = keras.Sequential([
 model_4g.compile(optimizer="adam",
                  loss=tf.keras.losses.mean_squared_error, metrics=["accuracy"])
 
-model_4g.fit(X, train_labels_4g, epochs=10)
+model_4g.fit(train_data_4g, train_labels_4g, epochs=10)
