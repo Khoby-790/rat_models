9ebeb5b7c19b64ea856acc1c3c09d7781a8f025a
Khoby-790 <ebaidoo79@gmail.com>
2021-02-16 17:51:39 +0000

    --- Auto-Git Commit ---

--- scripts/3g_model.py
+++ scripts/3g_model.py
@@ -12,5 +12,9 @@ import datetime
 
 four_g_data = pd.read_csv("../data/4g_data.csv")
 
+four_g_data = four_g_data[[""]]
+
+four_g_data.loc[(four_g_data.Test_type == "Upload"),"Test_type"] = 1
+four_g_data.loc[(four_g_data.Test_type == "Download"),"Test_type"] = 2
 
 print(four_g_data)
