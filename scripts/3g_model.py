bc6364ec3a1f5f929456e9e144bb78586ed24cd7
Khoby-790 <ebaidoo79@gmail.com>
2021-02-16 17:57:05 +0000

    --- Auto-Git Commit ---

--- scripts/3g_model.py
+++ scripts/3g_model.py
@@ -14,7 +14,20 @@ four_g_data = pd.read_csv("../data/4g_data.csv")
 
 four_g_data = pd.DataFrame(four_g_data)
 
-four_g_data.loc[(four_g_data.Test_type == "Upload"),"Test_type"] = 1
-four_g_data.loc[(four_g_data.Test_type == "Download"),"Test_type"] = 2
+four_g_data.loc[(four_g_data.Test_type == "Upload"), "Test_type"] = 1
+four_g_data.loc[(four_g_data.Test_type == "Download"), "Test_type"] = 2
+
+
+four_g_data["status"] = np.where(np.absolute(four_g_data["Signal_strength"]) > 85, 0, np.where(
+    np.absolute(four_g_data["Signal_strength"]) > 59, 1, np.where(np.absolute(four_g_data["Signal_strength"]) > 50,
+                                                                  2, np.where(np.absolute(
+                                                                      four_g_data["Signal_strength"]) > 40, 3, 4)
+                                                                  )
+))
+
 
 print(four_g_data)
+
+train_data_4g, test_data_4g = xlib.split_by_fractions(four_g_data, [0.8, 0.2])
+train_labels_4g = train_data_4g["status"]
+test_labels_4g = test_data_4g["status"]
