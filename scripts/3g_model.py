7a3039cd6b418a1b13127465afb8c3f6b8423946
Khoby-790 <ebaidoo79@gmail.com>
2021-02-16 18:03:38 +0000

    --- Auto-Git Commit ---

--- scripts/3g_model.py
+++ scripts/3g_model.py
@@ -17,10 +17,14 @@ four_g_data = pd.DataFrame(four_g_data)
 
 four_g_data["Test_type"] = np.where(
     (four_g_data.Test_type == 'Upload'), 1, four_g_data.Test_type)
+four_g_data["Technology"] = np.where(
+    (four_g_data.Test_type == '4G'), 1, four_g_data.Test_type)
 four_g_data["Test_type"] = np.where(
     (four_g_data.Test_type == 'Download'), 2, four_g_data.Test_type)
+four_g_data["Technology"] = np.where(
+    (four_g_data.Test_type == '3G'), 2, four_g_data.Test_type)
 
-
+print(four_g_data)
 
 four_g_data["status"] = np.where(np.absolute(four_g_data["Signal_strength"]) > 85, 0, np.where(
     np.absolute(four_g_data["Signal_strength"]) > 59, 1, np.where(np.absolute(four_g_data["Signal_strength"]) > 50,
@@ -29,8 +33,6 @@ four_g_data["status"] = np.where(np.absolute(four_g_data["Signal_strength"]) > 8
                                                                   )
 ))
 
-
-print(four_g_data)
 
 train_data_4g, test_data_4g = xlib.split_by_fractions(four_g_data, [0.8, 0.2])
 train_labels_4g = train_data_4g["status"]
