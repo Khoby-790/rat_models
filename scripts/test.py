5884e226fc395a6558633af0896d8d615f400455
Khoby-790 <ebaidoo79@gmail.com>
2021-02-16 16:47:19 +0000

    --- Auto-Git Commit ---

--- scripts/test.py
+++ scripts/test.py
@@ -16,11 +16,11 @@ threeG_data = actual_set[actual_set["Technology"] == "3G"]
 fourG_data = fourG_data[fourG_data["Signal_strength"] != "na"]
 threeG_data = actual_set[actual_set["Signal_strength"] != "na"]
 
-data_4g = np.array(fourG_data["Signal_strength"])
+# data_4g = np.array(fourG_data["Signal_strength"])
 
-fourG_data["Signal_strength"] = pd.Series(fourG_data["Signal_strength"]).abs()
+# fourG_data["Signal_strength"] = pd.Series(fourG_data["Signal_strength"]).abs()
 
-print(fourG_data["Signal_strength"])
+print(fourG_data["Signal_strength"].abs())
 
 # threeG_data.to_csv("../data/3g_data.csv")
 # fourG_data.to_csv("../data/4g_data.csv")
