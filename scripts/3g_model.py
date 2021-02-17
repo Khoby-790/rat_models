e2717bf3eaa3a1ff1d4bb04eebcb56b26b9a0c06
Khoby-790 <ebaidoo79@gmail.com>
2021-02-17 15:28:37 +0000

    --- Auto-Git Commit ---

--- scripts/3g_model.py
+++ scripts/3g_model.py
@@ -69,7 +69,7 @@ model_4g.compile(optimizer="adam",
 train_data_4g = np.asarray(train_data_4g).astype(np.int)
 train_labels_4g = np.asarray(train_labels_4g).astype(np.int)
 
-model_4g.fit(train_data_4g, train_labels_4g, epochs=10, steps_per_epoch=2300)
+model_4g.fit(train_data_4g, train_labels_4g, epochs=10)
 
 test_data_4g = np.asarray(test_data_4g).astype(np.int)
 test_labels_4g = np.asarray(test_labels_4g).astype(np.int)
