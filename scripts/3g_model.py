ae7e404e01e7a96e5e5b272cb77a6546aede19ca
Khoby-790 <ebaidoo79@gmail.com>
2021-02-17 10:56:48 +0000

    --- Auto-Git Commit ---

--- scripts/3g_model.py
+++ scripts/3g_model.py
@@ -18,11 +18,11 @@ four_g_data = pd.DataFrame(four_g_data)
 four_g_data["Test_type"] = np.where(
     (four_g_data.Test_type == 'Upload'), 1, four_g_data.Test_type)
 four_g_data["Technology"] = np.where(
-    (four_g_data.Test_type == '4G'), 1, four_g_data.Test_type)
+    (four_g_data.Technology == '4G'), 1, four_g_data.Technology)
 four_g_data["Test_type"] = np.where(
     (four_g_data.Test_type == 'Download'), 2, four_g_data.Test_type)
 four_g_data["Technology"] = np.where(
-    (four_g_data.Test_type == '3G'), 2, four_g_data.Test_type)
+    (four_g_data.Technology == '3G'), 2, four_g_data.Technology)
 
 # print(four_g_data)
 
