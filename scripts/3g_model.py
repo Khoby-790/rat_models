16ca4703da80a41844ceca834494690a008e3b5e
Khoby-790 <ebaidoo79@gmail.com>
2021-02-17 15:14:27 +0000

    --- Auto-Git Commit ---

--- scripts/3g_model.py
+++ scripts/3g_model.py
@@ -29,46 +29,44 @@ four_g_data["Technology"] = np.where(
 
 # print(four_g_data)
 
-four_g_data["status"] = np.where(np.absolute(
-    four_g_data["Signal_strength"]) <= 85, 1, 0
-)
+four_g_data["status"] = np.where(four_g_data["Signal_strength"] >= -85, 1, 0)
 
 
-head=len(four_g_data) * 0.7
-tail=len(four_g_data) * 0.3
+head = len(four_g_data) * 0.7
+tail = len(four_g_data) * 0.3
 
-X=four_g_data[["Signal_strength", "Test_type", "Data Speed(Mbps)"]]
-Y=four_g_data[["status"]]
-train_data_4g=X.head(int(head))
-test_data_4g=X.tail(int(tail))
-train_labels_4g=Y.head(int(head))
-test_labels_4g=Y.tail(int(tail))
+X = four_g_data[["Signal_strength", "Test_type", "Data Speed(Mbps)"]]
+Y = four_g_data[["status"]]
+train_data_4g = X.head(int(head))
+test_data_4g = X.tail(int(tail))
+train_labels_4g = Y.head(int(head))
+test_labels_4g = Y.tail(int(tail))
 
 print("before Array")
 print(train_data_4g)
 
-train_data_4g=np.array(train_data_4g)
+train_data_4g = np.array(train_data_4g)
 
 print("After Array")
 print(train_data_4g)
 
-normalize=preprocessing.Normalization()
+normalize = preprocessing.Normalization()
 normalize.adapt(train_data_4g)
 
 print("After normalize")
 print(train_labels_4g)
 
 print("Creating new 4G model")
-model_4g=keras.Sequential([
+model_4g = keras.Sequential([
     normalize,
     layers.Dense(16, activation="relu"),
     layers.Dense(18, activation="softmax"),
     layers.Dense(3, activation="softmax"),
 ])
-model_4g.compile(optimizer = "adam",
-                 loss = tf.keras.losses.mean_squared_error, metrics = ["accuracy"])
+model_4g.compile(optimizer="adam",
+                 loss=tf.keras.losses.mean_squared_error, metrics=["accuracy"])
 
-train_data_4g=np.asarray(train_data_4g).astype(np.int)
-train_labels_4g=np.asarray(train_labels_4g).astype(np.int)
+train_data_4g = np.asarray(train_data_4g).astype(np.int)
+train_labels_4g = np.asarray(train_labels_4g).astype(np.int)
 
-model_4g.fit(train_data_4g, train_labels_4g, epochs = 100)
+model_4g.fit(train_data_4g, train_labels_4g, epochs=10)
